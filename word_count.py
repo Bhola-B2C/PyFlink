5eb2f674089b195029d061e62aba6c73960f2b15
Bhola Nath Chowdhary <bholajrs@gmail.com>
2020-08-19 18:06:56 +0530

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -1,45 +1,27 @@
-################################################################################
-#  Licensed to the Apache Software Foundation (ASF) under one
-#  or more contributor license agreements.  See the NOTICE file
-#  distributed with this work for additional information
-#  regarding copyright ownership.  The ASF licenses this file
-#  to you under the Apache License, Version 2.0 (the
-#  "License"); you may not use this file except in compliance
-#  with the License.  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-# limitations under the License.
-################################################################################
 import logging
 import os
 import shutil
 import sys
 import tempfile
 
+from add import add
+import wikipedia
+
 from pyflink.dataset import ExecutionEnvironment
-from pyflink.table import BatchTableEnvironment, TableConfig
-from pyflink.table.descriptors import FileSystem, OldCsv, Schema
-from pyflink.table.types import DataTypes
+from pyflink.table import TableConfig, DataTypes, BatchTableEnvironment
+from pyflink.table.descriptors import Schema, OldCsv, FileSystem
 
 
 def word_count():
-    content = "line Licensed to the Apache Software Foundation ASF under one " \
-              "line or more contributor license agreements See the NOTICE file " \
-              "line distributed with this work for additional information " \
-              "line regarding copyright ownership The ASF licenses this file " \
-              "to you under the Apache License Version the " \
-              "License you may not use this file except in compliance " \
-              "with the License"
+    result = wikipedia.page("New York City")
+    content = result.summary
+
 
     t_config = TableConfig()
     env = ExecutionEnvironment.get_execution_environment()
     t_env = BatchTableEnvironment.create(env, t_config)
-
+    print(add.add(10,5))
+    print("Word Count");
     # register Results table in table environment
     tmp_dir = tempfile.gettempdir()
     result_path = tmp_dir + '/result'
@@ -54,15 +36,26 @@ def word_count():
 
     logging.info("Results directory: %s", result_path)
 
-    t_env.connect(FileSystem().path(result_path)) \
-        .with_format(OldCsv()
-                     .field_delimiter(',')
-                     .field("word", DataTypes.STRING())
-                     .field("count", DataTypes.BIGINT())) \
-        .with_schema(Schema()
-                     .field("word", DataTypes.STRING())
-                     .field("count", DataTypes.BIGINT())) \
-        .register_table_sink("Results")
+    #sink_ddl = """
+    #    create table Results(
+    #        word VARCHAR,
+    #        `count` BIGINT
+    #    ) with (
+    #        'connector.type' = 'filesystem',
+    #        'format.type' = 'csv',
+    #        'connector.path' = '{}'
+    #   )
+    #    """.format(result_path)
+    t_env.connect(FileSystem().path('/tmp/output')) \
+    .with_format(OldCsv()
+                 .field_delimiter('\t')
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .with_schema(Schema()
+                 .field('word', DataTypes.STRING())
+                 .field('count', DataTypes.BIGINT())) \
+    .create_temporary_table('Results')
+    #t_env.sql_update(sink_ddl)
 
     elements = [(word, 1) for word in content.split(" ")]
     t_env.from_elements(elements, ["word", "count"]) \
@@ -73,7 +66,7 @@ def word_count():
     t_env.execute("word_count")
 
 
-if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+#if __name__ == '__main__':
+#    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
 
-    word_count()
+ #   word_count()
